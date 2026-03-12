8fc3f6314b3241cb2f65cc84fe2dfc7d46cbdad2
Artur Magakyan <magakyanartur@gmail.com>
2026-03-12 09:29:29 +0300

    refactor(shop): add Customer interface, Faker and Allure tags

--- tests/shop.spec.ts
+++ tests/shop.spec.ts
@@ -1,8 +1,22 @@
 import { test } from './fixtures';
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
+import * as allure from 'allure-js-commons';
+
+interface Customer {
+  firstName: string;
+  lastName: string;
+  zipCode: string;
+}
 
 test.describe('E2E Shopping Flow', () => {
   test('User can complete a purchase', async ({ loginPage, inventoryPage, cartPage, checkoutPage }) => {
+
+    await allure.epic('E-Commerce Core'); 
+    await allure.feature('Checkout Process'); 
+    await allure.story('Successful purchase with valid fake data'); 
+    await allure.tags('e2e', 'critical_path'); 
+    await allure.severity('blocker'); 
+
     await loginPage.goto();
     await loginPage.login('standard_user', process.env.SAUCE_PASSWORD || 'secret_sauce');
 
@@ -12,13 +26,19 @@ test.describe('E2E Shopping Flow', () => {
     await cartPage.checkItemVisible('Sauce Labs Backpack');
     await cartPage.clickCheckout();
     
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    const zipCode = faker.location.zipCode();
+    const randomCustomer: Customer ={
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      zipCode: faker.location.zipCode()
+    };
 
-    await checkoutPage.fillDetails(firstName,lastName,zipCode);
-    await checkoutPage.finishOrder();
+    await checkoutPage.fillDetails(
+      randomCustomer.firstName,
+      randomCustomer.lastName,
+      randomCustomer.zipCode
+    );
     
+    await checkoutPage.finishOrder();
     await checkoutPage.expectSuccessMessage();
   });
 });
